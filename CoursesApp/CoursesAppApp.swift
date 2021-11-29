bb758b03d58ff09d715ad3939150dd08ba6fd5b9
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-11-29 14:50:11 +0200

    Initial Commit

---
+++ CoursesApp/CoursesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesAppApp.swift
+//  CoursesApp
+//
+//  Created by Amr Hesham on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoursesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
